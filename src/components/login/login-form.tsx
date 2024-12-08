8fe7c7a1494e037aedc63e2b7572bcfa41b1d567
Maycon Kabitschke <mayconkabitschke@gmail.com>
2024-12-08 11:14:16 -0300

    useForm

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -1,12 +1,34 @@
+'use client'
 import login from "@/actions/login";
+import { useFormState, useFormStatus } from "react-dom";
+import Button from "@/components/forms/button";
+import React from "react";
+//DEU erro useFormState substitui por  React.useActionState
+
+function FormButton() {
+  const { pending } = useFormStatus();
+  return <>{pending ? <Button disabled={pending}>Enviando</Button> :
+    <Button>Entrar</Button>
+  }</>
+}
 
 export default function LoginForm() {
+  const [state, action] = React.useActionState(login, {
+    ok: false,
+    error: '',
+    data: null
+  });
+
+
+
+
   return (
     <>
-      <form action={login}>
+      <form action={action}>
         <input type="text" name="username" placeholder="Usuário" />
         <input type="password" name="password" placeholder="Senha" />
-        <button>Entrar</button>
+        <FormButton />
+        <p style={{ color: 'red' }}>{state.error}</p>
       </form>
 
     </>
