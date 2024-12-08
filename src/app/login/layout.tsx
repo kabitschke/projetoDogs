091f3238f28c9c1863464a498428c812cd7fe6f8
Maycon Kabitschke <mayconkabitschke@gmail.com>
2024-12-08 09:42:51 -0300

    login layout

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,8 @@
+import styles from './login.module.css';
+export default function LoginLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className={styles.login}>
+      <div className={styles.forms}>{children}</div>
+    </div>
+  );
+}
