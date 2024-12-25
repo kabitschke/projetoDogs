da20c2862aba71687215a2ccf4e2932e9388a2e5
Maycon Kabitschke <mayconkabitschke@gmail.com>
2024-12-25 13:52:09 -0300

    Conta Header

--- src/app/conta/page.tsx
+++ src/app/conta/page.tsx
@@ -6,10 +6,10 @@ export default async function ContaPage() {
 
 
   const { user } = useUser();
-  console.log(user);
+
   return (
     <div>
-      <h1 onClick={() => console.log('teste')}>Conta:{user?.nome}</h1>
+      <h1 >Conta:{user?.nome}</h1>
 
     </div>
   );
