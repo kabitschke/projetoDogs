59f8fb9ca09eff645a4ba431c4c1b8348d15fb29
Maycon Kabitschke <mayconkabitschke@gmail.com>
2024-12-26 18:05:30 -0300

    loading

--- src/components/feed/feed.tsx
+++ src/components/feed/feed.tsx
@@ -2,6 +2,8 @@
 import photosGet, { Photo } from '@/actions/photos-get';
 import FeedPhotos from './feed-photos';
 import React from 'react';
+import Loading from '@/components/helper/loading';
+import styles from './feed.module.css'
 
 export default function Feed({ photos, user }: { photos: Photo[], user?: 0 | string }) {
   const [photosFeed, setPhotosFeed] = React.useState<Photo[]>(photos);
@@ -58,7 +60,10 @@ export default function Feed({ photos, user }: { photos: Photo[], user?: 0 | str
   return (
     <div>
       <FeedPhotos photos={photosFeed} />
-      {loading && <p>Carregando...</p>}
+      <div className={styles.loadingWrapper}>
+        {infinite ? loading && <Loading /> : <p>Não existem mais postagens</p>}
+
+      </div>
     </div>
   );
 }
