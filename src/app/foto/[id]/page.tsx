43f7b8dc91911933975fd791c851ce48c8049a40
Maycon Kabitschke <mayconkabitschke@gmail.com>
2024-12-28 10:22:21 -0300

    photo content

--- src/app/foto/[id]/page.tsx
+++ src/app/foto/[id]/page.tsx
@@ -1,3 +1,38 @@
-export default async function FotoIDPage({ params }: { params: { id: number } }) {
-  return <main><h1>Foto id: {params.id}</h1></main>
+import photoGet from "@/actions/photo-get";
+import PhotoContent from "@/components/photo/photo-content";
+import { notFound } from "next/navigation";
+
+
+type FotoIdParams = {
+  params: {
+    id: string;
+  };
+};
+
+export async function generateMetadata({ params }: FotoIdParams) {
+  const { data } = await photoGet(params.id);
+
+  if (!data) return { title: 'Fotos' };
+  return {
+    title: data.photo.title,
+  };
+}
+
+
+
+
+export default async function FotoIDPage({ params }: FotoIdParams) {
+
+  const { data } = await photoGet(params.id);
+
+
+  if (!data) return notFound();
+  return (
+    <section className="container mainContainer">
+      <PhotoContent data={data} single={true} />
+      <h1>Foto id: {data.photo.title}</h1>
+    </section>
+
+  );
+
 }
