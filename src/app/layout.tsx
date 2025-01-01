ebccf75f53840f14a0dda8eb593d530b095855cd
Maycon Kabitschke <mayconkabitschke@gmail.com>
2025-01-01 08:36:35 -0300

    rota paralela

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,24 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 import { type_second } from '@/functions/fonts';
-import Header from "@/components/header";
-import Footer from "@/components/footer";
-import { UserContextProvider } from "@/context/user-context";
-import userGet from "@/actions/user-get";
+import Header from '@/components/header';
+import Footer from '@/components/footer';
+import { UserContextProvider } from '@/context/user-context';
+import userGet from '@/actions/user-get';
 
 export const metadata: Metadata = {
-  title: "Dogs Next",
-  description: "Rede Social para cachorros.",
-  icons: {
-    icon: "/favicon.ico",
-  },
+  title: 'Dogs Next',
+  description: 'Rede social para cachorros.',
 };
 
 export default async function RootLayout({
   children,
+  modal,
 }: Readonly<{
   children: React.ReactNode;
+  modal: React.ReactNode;
 }>) {
-
-
-
   const { data: user } = await userGet();
-
 
   return (
     <html lang="pt-BR">
@@ -31,9 +26,8 @@ export default async function RootLayout({
         <UserContextProvider user={user}>
           <div className="App">
             <Header />
-            <main className="AppBody">
-              {children}
-            </main>
+            <main className="AppBody">{children}</main>
+            <div>{modal}</div>
             <Footer />
           </div>
         </UserContextProvider>
